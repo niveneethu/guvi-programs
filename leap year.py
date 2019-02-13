b9282c10c177455349a55e3a2d3a620b511a803d
niveneethu <47175425+niveneethu@users.noreply.github.com>
2019-02-13 11:27:57 +0530

    Create leap year.py

---
+++ leap year.py
@@ -0,0 +1,11 @@
+year = int(input())
+
+# Leap Year Check
+if year % 4 == 0 and year % 100 != 0:
+    print("yes")
+elif year % 100 == 0:
+    print("no")
+elif year % 400 ==0:
+    print("yes")
+else:
+    print("no")
